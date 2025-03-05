a61c8ca4b7b956b533903f9274c1f7b4c108267a
ProgramacaoGH <wevanyfrancisco@gmail.com>
2025-03-05 18:51:37 +0000

    finalizado

--- cartasSuperTrunfo.c
+++ cartasSuperTrunfo.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 int main(){
-    char estado;
+    char estado[50];
     char codigo[50];
-    char cidade;
+    char cidade[50];
     int populacao;
     float area;
     float pib;
@@ -30,13 +30,13 @@ int main(){
     printf("Quantidade de Ponto Turisticos: \n");
     scanf("%d", &pontoTuristico);
 
-    printf("");
-    printf("");
-    printf("");
-    printf("");
-    printf("");
-    printf("");
-    printf("");
+    printf("Estado: %s\n", estado);
+    printf("Código: %d\n", codigo);
+    printf("Cidade: %s\n", cidade);
+    printf("População: %d\n", populacao);
+    printf("Área: %f\n", area);
+    printf("PIB: %d\n", pib);
+    printf("Quantida de Ponto Turistico: %d\n", pontoTuristico);
 
     return 0;
 }
